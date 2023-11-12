b0ec750f7e6acc16bc27db7758d31f76d501ab8e
M. Babenko <babenoff.code@outlook.com>
2023-11-12 20:04:00 +0300

    build(MainApp): Базовая сбора приложения

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/babenow/newsbot/internal/config"
+	"github.com/babenow/newsbot/internal/fetcher"
+	"github.com/babenow/newsbot/internal/notifier"
+	"github.com/babenow/newsbot/internal/storage"
+	"github.com/babenow/newsbot/internal/summary"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	botApi, err := tgbotapi.NewBotAPI(config.Get().TelegramBotToken)
+	if err != nil {
+		log.Printf("[ERROR] failed to create telegam bot: %v", err)
+		return
+	}
+
+	db, err := sqlx.Connect("postgres", config.Get().DatabaseDNS)
+	if err != nil {
+		log.Printf("[ERROR] failed to connect database: %v", err)
+		return
+	}
+	defer db.Close()
+	var (
+		aStorage = storage.NewArticlePostgresStorage(db)
+		sStorage = storage.NewSourcePostgresStorage(db)
+		fetcher  = fetcher.NewFetcher(
+			aStorage,
+			sStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+		notifier = notifier.NewNotifier(
+			aStorage,
+			summary.NewOpenAISummarizer(config.Get().OpenAIKey, config.Get().OpenAIPrompt),
+			botApi,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to start fetcher: %v", err)
+				return
+			}
+
+			log.Printf("[ERROR] fetcher stopped with error: %v", err)
+		}
+	}(ctx)
+
+	// go func(ctx context.Context) {
+	if err := notifier.Start(ctx); err != nil {
+		if !errors.Is(err, context.Canceled) {
+			log.Printf("[ERROR] failed to select and send articles: %v", err)
+			return
+		}
+
+		log.Printf("[ERROR] notifier stopped with error^ %v", err)
+	}
+	// }(ctx)
 }
